6608f502033d3e7af9147a5891f275e9a9f91e7a
linlak <nowomukama.linlak.linus@gmail.com>
2019-03-17 05:23:24 -0700

    save

--- src/Bootstraper.php
+++ src/Bootstraper.php
@@ -10,7 +10,7 @@ class Bootstraper extends Database
 		
 	private $environ="sandbox";
 	private $dbName="";
-	private $tables=['momo_api_user','momo_payment_queue'];
+	private $tables=['momo_api_user','momo_payment_queue','momo_access_tokens'];
 	private $found_tables=[];
 	function __construct($host,$dbName,$dbUser,$dbPass,$environ='sandbox')
 	{
@@ -38,7 +38,8 @@ class Bootstraper extends Database
 					  `admin_post` varchar(255) NOT NULL,
 					  `lastVisted` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 					  `admin_level` enum('a','b','c','d') NOT NULL DEFAULT 'a'
-					) ENGINE=InnoDB DEFAULT CHARSET=armscii8;CREATE TABLE IF NOT EXISTS `momo_payment_queue` (
+					) ENGINE=InnoDB DEFAULT CHARSET=armscii8;
+				CREATE TABLE IF NOT EXISTS `momo_payment_queue` (
 					  `admin_id` int(11) NOT NULL,
 					  `user_id` int(11) NOT NULL,
 					  `admin_post` varchar(255) NOT NULL,
@@ -66,34 +67,40 @@ class Bootstraper extends Database
 					  `uuid` varchar(100) NOT NULL,
 					  `api_primary` varchar(255) NOT NULL,
 					  `api_secondary` varchar(255) NOT NULL,
-					  `access_token` TEXT NULL,
 					  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 					  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 					  `api_key` varchar(255)  NULL
-					) ENGINE=InnoDB DEFAULT CHARSET=armscii8; 
+					) ENGINE=InnoDB DEFAULT CHARSET=armscii8;
+
+					CREATE TABLE  `momo_access_tokens` (
+					  `uuid` varchar(100) NOT NULL,
+					  `access_token` TEXT NULL,
+					  `token_type` TEXT NULL,
+					  `expires_in`  int(11) NOT NULL DEFAULT 0,
+					  `started_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+					  `expires_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+					) ENGINE=InnoDB DEFAULT CHARSET=armscii8;
 
 				ALTER TABLE `momo_api_user` 
 				ADD PRIMARY KEY (`uuid`),
 				ADD UNIQUE KEY `api_primary` (`api_primary`),
 				ADD UNIQUE KEY `api_secondary` (`api_secondary`),
 				ADD UNIQUE `unique_user` (`api_primary`, `api_secondary`) USING BTREE;
+
+				ALTER TABLE `momo_access_tokens` 
+				ADD PRIMARY KEY (`uuid`);
+
+				ALTER TABLE `momo_access_tokens`
+  				ADD CONSTRAINT `fk_momo_token_api_uuid` FOREIGN KEY (`uuid`) REFERENCES `momo_api_user` (`uuid`) ON DELETE CASCADE ON UPDATE CASCADE;
 			";
-
-			if (in_array('momo_payment_queue', $this->found_tables)) {
-					$sql.=" 
-
-						CREATE TABLE  `momo_payment_queue` (
-							  ``
-							  `uuid` varchar(100) NOT NULL,
-							  `api_primary` varchar(255) NOT NULL,
-							  `api_secondary` varchar(255) NOT NULL,
-							  `access_token` TEXT NULL,
-							  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-							  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-							  `api_key` varchar(255)  NULL
-							) ENGINE=InnoDB DEFAULT CHARSET=armscii8; 
-					";
-			}
+			$sql.=$this->paymentsTable('momo_payment_queue','queue');
+						
+		}else{
+			$sql.=$this->paymentsTable('momo_payment_queue','queue');
+			// $sql.=$this->paymentsTable('momo_payment_queue','queue');
+			// $sql.=$this->paymentsTable('momo_payment_queue','queue');
+			// $sql.=$this->paymentsTable('momo_payment_queue','queue');
 		}
 		
 echo "<pre>";
@@ -103,5 +110,37 @@ echo($sql);
 			// $this->debugDumpParams();
 		// print_r(array_values($this->found_tables));
 
+	}
+
+	private function paymentsTable($tbName,$fkPrefix){
+		$sql="";
+		if (!in_array($tbName, $this->found_tables)) {
+				$sql.=" 
+
+						CREATE TABLE  `$tbName` (
+							  `referenceId` varchar(100) NOT NULL, 
+							  `uuid` varchar(100) NOT NULL,
+							  `amount` varchar(255) NOT NULL,
+							  `partyIdType` varchar(255) NOT NULL,
+							  `partyId` varchar(255) NOT NULL,
+							  `currency` varchar(255) NOT NULL,
+							  `externalId` varchar(100) NOT NULL,
+							  `payerMessage` TEXT  NULL,
+							  `payeeNote` TEXT  NULL,
+							  `status` varchar(20) NOT NULL DEFAULT 'PENDING',
+							  `reason` TEXT  NULL,
+							  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+							  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+							) ENGINE=InnoDB DEFAULT CHARSET=armscii8; 
+
+							ALTER TABLE `$tbName` 
+							ADD PRIMARY KEY (`referenceId`),
+							ADD KEY `uuid` (`uuid`);
+
+							ALTER TABLE `momo_access_tokens`
+  							ADD CONSTRAINT `fk_momo_".$fkPrefix."_api_uuid` FOREIGN KEY (`uuid`) REFERENCES `momo_api_user` (`uuid`) ON DELETE CASCADE ON UPDATE CASCADE;
+					";	
+			}
+			return $sql;
 	}
 }
--- src/Models/RequestStatus.php
+++ src/Models/RequestStatus.php
@@ -1,26 +1,4 @@
 <?php
-/*Array
-(
-    [status_code] => 200
-    [status_phrase] => OK
-    [data] => Array
-        (
-            [externalId] => 3940bb2d-73d1-4067-b0d1-eded3221d935
-            [amount] => 1000
-            [currency] => EUR
-            [payer] => Array
-                (
-                    [partyIdType] => MSISDN
-                    [partyId] => 46733123450
-                )
-
-            [payerMessage] => Testing
-            [payeeNote] => Testing
-            [status] => FAILED
-            [reason] => INTERNAL_PROCESSING_ERROR
-        )
-
-)*/
 
 class RequestStatus extends MomoResponse
 {
@@ -35,7 +13,7 @@ class RequestStatus extends MomoResponse
     private $status="";
 	private $reason="";
 	function __construct(array $response){
-		
+		parent::__construct($response);
 	}
 
 	public function getStatus(){
